e8bf673ce708234faf5b5c1a7498481d314abbdd
kishan <kishan.1998.v@gmail.com>
2025-03-08 04:45:08 +0530

    Setup of React App using Vite and Tailwind Setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>Welcome</div>
+      <div className="min-h-screen bg-blue-500 text-white flex items-center justify-center text-3xl">
+      Tailwind is working! 🚀
+    </div>
+    </>
+  )
+}
+
+export default App
